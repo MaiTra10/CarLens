1fe6baaf2b82c7b344421248cb317c9d09d0341c
Maitra Patel <10maitra22@gmail.com>
2025-03-17 21:02:13 -0600

    msc - unnecessary comments in server.go

--- api/server/server.go
+++ api/server/server.go
@@ -6,14 +6,13 @@ import (
 
 	ai "github.com/MaiTra10/CarLens/api/routes/AI"
 	"github.com/MaiTra10/CarLens/api/routes/user"
-	// "github.com/MaiTra10/CarLens/api/routes/AI"
 )
 
 func StartServer() {
 
 	// Define our routes
 	http.HandleFunc("/login", user.LoginHandler)
-	// http.HandleFunc("/logout", user.LogoutHandler)
+	http.HandleFunc("/logout", user.LogoutHandler)
 	http.HandleFunc("/register", user.RegisterHandler)
 
 	http.HandleFunc("/ai", ai.AIHandler)
