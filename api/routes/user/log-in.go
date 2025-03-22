7193143cc7507cc538acc809de1777831638ae76
Maitra Patel <10maitra22@gmail.com>
2025-03-22 02:24:08 -0600

    test

--- api/routes/user/log-in.go
+++ api/routes/user/log-in.go
@@ -71,9 +71,9 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// w.Header().Set("Access-Control-Allow-Origin", "http://18.237.85.135:3000")
-	// w.Header().Set("Access-Control-Allow-Credentials", "true")
-	// w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 	http.SetCookie(w, &http.Cookie{
 		Name:     "access_token",
